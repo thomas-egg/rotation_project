e38bf8a80838ad3c94390797c2c6c4aac1169b27
thomas-egg <tje3676@nyu.edu>
2023-10-06 13:41:05 -0400

    Created my own TAMD

--- tripeptoid_tamd_ggmt.py
+++ tripeptoid_tamd_ggmt.py
@@ -85,3 +85,51 @@ def dihedral_angle_cvs(prmtop_file, name, *atom_types):
         
         # Return CVs
         return collective_variables
+
+# MAIN
+if __name__ == '__main__':
+    
+    # Seed process
+    seed = random.SystemRandom().randint(0, 2**31) if args.seed is None else args.seed
+
+    # Create system
+    inpcrd = app.AmberInpcrdFile(f'{args.case}.inpcrd')
+    prmtop = app.AmberPrmtopFile(f'{args.case}.prmtop')
+
+    system = prmtop.createSystem(
+        nonbondedMethod=app.NoCutoff if prmtop.topology.getNumChains() == 1 else app.PME,
+        implicitSolvent=app.GBn2,
+        implicitSolventSaltConc=args.salt_molarity*unit.moles/unit.liter,
+        constraints=None,
+        rigidWater=True,
+        removeCMMotion=False
+    )
+
+    # Phi, psi, and omega angles
+    phi_angles = dihedral_angle_cvs(f'{args.case}.prmtop', 'phi', 'c', 'n', 'c3', 'c')
+    s_phi_1 = ufedmm.DynamicalVariable('s_phi_1', -limit, limit, mass, Ts, phi_angles[0], Ks, sigma=None)
+    s_phi_2 = ufedmm.DynamicalVariable('s_phi_2', -limit, limit, mass, Ts, phi_angles[1], Ks, sigma=None)
+
+    psi_angles = dihedral_angle_cvs(f'{args.case}.prmtop', 'psi', 'n', 'c3', 'c', 'n')
+    s_psi_1 = ufedmm.DynamicalVariable('s_psi_1', -limit, limit, mass, Ts, psi_angles[0], Ks, sigma=None)
+    s_psi_2 = ufedmm.DynamicalVariable('s_psi_2', -limit, limit, mass, Ts, psi_angles[1], Ks, sigma=None)
+
+    omega_angles = dihedral_angle_cvs(f'{args.case}.prmtop', 'omega', 'c3', 'c', 'n', 'c3')
+    s_omega_1 = ufedmm.DynamicalVariable('s_omega_1', -limit, limit, mass, Ts, omega_angles[2], Ks, sigma=None)
+
+    # Setup simulation
+    ufed = ufedmm.UnifiedFreeEnergyDynamics([s_phi_1,s_phi_2,s_psi_1,s_psi_2,s_omega_1], temp)
+    ufedmm.serialize(ufed, 'ufed_object.yml')
+    integrator = ufedmm.MiddleMassiveGGMTIntegrator(temp,40*unit.femtoseconds, dt, scheme='VV-Middle')
+    integrator.setRandomNumberSeed(seed)
+    platform = openmm.Platform.getPlatformByName(args.platform)
+    simulation = ufed.simulation(prmtop.topology, system, integrator, platform)
+    simulation.context.setPositions(inpcrd.positions)
+    simulation.context.setVelocitiesToTemperature(temp, seed)
+    output1 = ufedmm.Tee(stdout, 'COLVAR')
+    reporter1 = ufedmm.StateDataReporter(output1,10, step=True, multipleTemperatures=False,hillHeights=False ,variables=True,speed=True,separator='\t')
+    simulation.reporters.append(reporter1)
+
+    # Run simulation
+    simulation.minimizeEnergy()
+    simulation.step(nsteps)
